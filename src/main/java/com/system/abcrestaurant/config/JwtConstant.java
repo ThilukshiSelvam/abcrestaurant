9e4feda83b8169e9a86aba8037df9bb7cefa717c
ThilukshiSelvam <thilukshiselvam@gmail.com>
2024-08-29 06:45:23 +0530

    commit config

---
+++ src/main/java/com/system/abcrestaurant/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.system.abcrestaurant.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY="sdfghjekflerguherjgoejgoifjokfowfjibbuerhyewrtyt";
+    public static final String JWT_HEADER="Authorization";
+}
