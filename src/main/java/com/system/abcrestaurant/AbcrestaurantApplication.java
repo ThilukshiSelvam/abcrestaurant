4bf5d8f8da2c54ba50a4e2ae21d80aa6679a559d
ThilukshiSelvam <thilukshiselvam@gmail.com>
2024-08-29 06:04:29 +0530

    Project First Commit

---
+++ src/main/java/com/system/abcrestaurant/AbcrestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.system.abcrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcrestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcrestaurantApplication.class, args);
+	}
+
+}
